bfcfe024e057b8c1894a7bd7d73404d94bc17620
MArndt26 <MArndt26@users.noreply.github.com>
2022-06-28 00:14:59 +0000

    deploy: 88245079f8fc5d939b97126affc036b2e737f584

--- _next/static/Q2XhqQbQOBZ---IFGhh29/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-541c1307a2888d59.js","static/chunks/831-12c359d23567e355.js","static/chunks/pages/index-6b6480f69b770e60.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
