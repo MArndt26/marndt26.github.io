1f442ccb251ad48986f3996bc191b3b19ff7dfa3
MArndt26 <MArndt26@users.noreply.github.com>
2022-07-01 20:46:37 +0000

    deploy: a0ca2817890ed613daacaf4343da91e4510849d0

---
+++ _next/static/Gl3eBfSqjIpEfK1fyNtGS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-541c1307a2888d59.js","static/chunks/831-12c359d23567e355.js","static/chunks/pages/index-3dc26b5b5907538b.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
