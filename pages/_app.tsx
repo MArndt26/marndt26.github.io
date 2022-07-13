a1c73f4d2e1bdd578ee2658ec20a9d57f0eea30c
Mitch Arndt - C70441 <Mitch.Arndt@microchip.com>
2022-07-13 14:23:31 -0700

    added google analytics to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,15 @@ import '../styles/globals.css';
 import '../styles/media.css';
 import '../styles/sacnite.css';
 import type { AppProps } from 'next/app';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
